6ef4ebfe4ccce66b9a9b1d9a3a46749b5e1608d4
Arthur Oliveira <88795235+arthuroliveirasou@users.noreply.github.com>
2021-08-11 17:54:13 -0300

    Update with Portuguese translation

--- common.js
+++ common.js
@@ -3,26 +3,31 @@ const LOCALIZE = {
         ko: '항목을 모두 입력하세요.',
         en: 'Please enter all fields.',
         zh_TW: '請輸入所有項目。',
+        pt: 'Por favor, preencha todos os campos.',
     },
     possibleAchieve: {
         ko: '목표 달성이 가능합니다.',
         en: 'Possible to achieve your goal.',
         zh_TW: '您可以實現自己的目標。',
+        pt: 'É possível alvançar sua meta.',
     },
     impossibleAchieve: {
         ko: '목표 달성이 불가능합니다.',
         en: 'Impossible to achieve your goal.',
         zh_TW: '無法實現目標。',
+        pt: 'Impossível alcançar sua meta.',
     },
     notSupportedYet: {
         ko: '아직 이번 시즌 대응 업데이트를 하지 않았습니다.',
         en: 'Not yet updated for latest season pass.',
         zh_TW: '尚未更新。',
+        pt: 'Ainda não atualizado para o último passe de temporada.',
     },
     notStartedYet: {
         ko: '아직 이번 시즌 패스 기간이 아닙니다.',
         en: 'Pass period not yet started.',
         zh_TW: '通行證術語還沒開始。',
+        pt: 'O período do passe ainda não começou.',
     }
 }
 
